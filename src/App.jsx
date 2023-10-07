b33c1ba0871548f2007cadb3bbf5ddba7f57cfe6
Nwodothomas <108361891+Nwodothomas@users.noreply.github.com>
2023-10-07 16:38:55 +0100

    Created pages and routed pages in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+
+import {BowserRouter, Routes, Route} from 'reat-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import Signup from './pages/Signup';
+import About from './pages/About';
+import Profile from './pages/Profile';
+
 
 export default function App() {
   return (
-    <div>
-      <h1 className='text-red-500'>App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/SignIn' element={<SignIn />} />
+        <Route path='/Signup' element={<Signup />} />
+        <Route path='/About' element={<About />} />
+        <Route path='/Profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
